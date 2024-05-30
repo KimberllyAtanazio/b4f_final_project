e404f9200dabc172051527ea588981bbbd54f069
KimberllyAtanazio <kimberlly.atanazio@gmail.com>
2024-05-30 23:45:01 +0100

    Finished Country Page

--- src/pages/countries/[title].js
+++ src/pages/countries/[title].js
@@ -1,31 +1,85 @@
 import { useRouter } from 'next/router';
 import paises from '@/../paises.json';
+import styles from "./detailsCountry.module.css"
+import { DetailCardIcon } from '@/../public/icons/DetailCardIcon';
+import { LocationIcon } from '@/../public/icons/LocationIcon';
+import { BackIcon } from '@/../public/icons/BackIcon';
+import NavBar from '@/components/NavBar';
 
-export default function Home() {
+export default function DetailsCountry() {
   const router = useRouter();
+  const { title } = router.query;
 
-  const pais = paises.find((e) => e.title == router.query.title);
+  if (!title) {
+    return <>LOADING</>;
+  }
+
+  const pais = paises.find((e) => e.title === title);
+
+  if (!pais) {
+    return <>LOADING</>;
+  }
 
   return (
-    <div>
-      <h1>{pais ? pais.about : <>LOADING</>}</h1>
-
+    <div className={styles.container}>
       <div>
-        {pais ? (
-          pais.curiosities.map((curiosity, index) => (
+          <button
+            className={styles.backButton}
+            onClick={() => router.back()}
+          >
+            <BackIcon />
+          </button>
+        <img
+          src={`/images/${pais.imageName}`}
+          alt={pais.title}
+          className={styles.image}
+        />
+
+      </div>
+      <div className={styles.card}>
+        <div className={styles.retangle}>
+          <DetailCardIcon />
+        </div>
+
+        <h1 className={styles.title}>{pais.title}</h1>
+
+        <div className={styles.continent}>
+          <LocationIcon />
+          <p> {pais.continent}</p>
+        </div>
+
+        {/* Tags: */}
+        <ul className={styles.listCard}>
+          {pais.tags.map((tag, index) => (
+            <li key={index} className={styles.listTags}>{tag}</li>
+          ))}
+        </ul>
+
+        <div className={styles.about}>
+          <h1>About Destination</h1>
+          <p>{pais.about}</p>
+        </div>
+
+        <div className={styles.curiosities}>
+          {pais.curiosities.map((curiosity, index) => (
             <div key={index}>
               <h2>{curiosity.topic}</h2>
               <p>{curiosity.description}</p>
             </div>
-          ))
-        ) : (
-          <>LOADING</>
-        )}
+          ))}
+        </div>
+
+        <div className={styles.placesBox}>
+          <h2 className={styles.places}>Must visit places:</h2>
+          <ul className={styles.placeList}>
+            {pais.must_visit_places.map((place, index) => (
+              <li key={index} className={styles.placesInfos}>{place}</li>
+            ))}
+          </ul>
+        </div>
       </div>
 
-      <h2>Must visit places:</h2>
-      <h3>{pais ? pais.must_visit_places : <>LOADING</>}</h3>
-      <h3>{pais ? pais.tags : <>LOADING</>}</h3>
+      <NavBar />
     </div>
   );
 }
--- src/pages/country.js
+++ src/pages/country.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import Header from '@/components/Header';
 import SearchBar from '@/components/SearchBar';
 import CountryCard from '@/components/Countries';
 import paises from '../../paises.json';
 import NavBar from '@/components/NavBar';
 import styles from "./country.module.css"
+import FilterButton from '@/components/FilterButton';
 
 export default () => {
+
+  const [continenteSelecionado, setContinenteSelecionado] = useState('todos');
+
+  const handleFiltroChange = (e) => {
+      setContinenteSelecionado(e.target.value);
+  };
+
+  const paisesFiltrados = continenteSelecionado === 'todos' ? paises : paises.filter(pais => pais.continent === continenteSelecionado);
+
   return (
     <div className={styles.container}>
         <div>
@@ -13,10 +24,11 @@ export default () => {
         </div>
         <div className={styles.searchForm}>
             <SearchBar />
+            <FilterButton onChange={handleFiltroChange} />
         </div>
       <div className={styles.countryCard}>
-        {paises.map((p) => (
-          <CountryCard country={p} />
+      {paisesFiltrados.map((p, index) => (
+          <CountryCard key={index} country={p} />
         ))}
       </div>
       <div>
